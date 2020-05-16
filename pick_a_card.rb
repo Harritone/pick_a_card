af19c10bd274d78c5bd9a6bda0cd91a8b3f7b1c4
Harritone <yourrasik@gmail.com>
2020-05-16 18:17:55 +1000

    with unicode symbols of suits now

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,4 +1,4 @@
 #comment
 values = ['2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K', 'A']
-suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
-puts "#{values.sample} of #{suits.sample}"
+suits = ["\u2660", "\u2665", "\u2666", "\u2663"]
+puts "#{values.sample} of #{suits.sample.encode('utf-8')}"
